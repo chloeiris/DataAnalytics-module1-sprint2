de4b9cacfe8504e7adb045d4446c22f7008d76ee
cs-pauli <107844952+cs-pauli@users.noreply.github.com>
2022-08-04 19:36:40 +0200

    ejercicio sql 13 hecho hasta el 5 no incluido (paula)

--- SQL/sql-modulo-1-sprint-2-ejercicio-13-consultas-multiples-tablas-5.sql
+++ SQL/sql-modulo-1-sprint-2-ejercicio-13-consultas-multiples-tablas-5.sql
@@ -19,17 +19,40 @@ WHERE order_date >= ALL(
 # Supongamos que ahora nuestro jefe quiere un informe de los productos más vendidos y su precio unitario.
 # De nuevo lo tendréis que hacer con queries correlacionadas.
 
-
-SELECT DISTINCT (product_id), unit_price, quantity 
+                    
+SELECT product_id, MAX(unit_price)
 FROM order_details AS A1
-GROUP BY A1.product_id
-HAVING SUM(A1.quantity) >= ALL (
-					SELECT SUM(A2.quantity)
+GROUP BY product_id
+HAVING MAX(A1.unit_price) >= ALL (
+					SELECT MAX(A2.unit_price)
                     FROM order_details AS A2
                     WHERE A1.product_id = A2.product_id);
                     
-MAX UNIT PRICE
+
+# EJERCICIO 3:
+# Ciudades que empiezan con "A" o "B".
+# Por un extraño motivo, nuestro jefe quiere que le devolvamos una tabla con aquelas compañias que están afincadas en ciudades
+# que empiezan por "A" o "B". Necesita que le devolvamos la ciudad, el nombre de la compañia y el nombre de contacto.
+
+SELECT city, company_name, contact_name
+FROM customers
+WHERE city LIKE 'A%' OR city LIKE 'B%'; 
 
 
+# EJERCICIO 4: 
+# Número de pedidos que han hecho en las ciudades que empiezan con L.
+# En este caso, nuestro objetivo es devolver los mismos campos que en la query anterior el número de total de pedidos que
+# han hecho todas las ciudades que empiezan por "L".
+
+SELECT city, company_name, contact_name, COUNT(order_id)
+FROM customers
+LEFT JOIN orders
+ON customers.customer_id = orders.customer_id 
+GROUP BY city, company_name, contact_name
+HAVING city LIKE 'L%';
 
 
+# EJERCICIO 5 
+# Todos los clientes cuyo "contact_title" no incluya "Sales".
+# Nuestro objetivo es extraer los clientes que no tienen el contacto "Sales" en su "contact_title". Extraer el nombre de 
+# contacto, su posisión (contact_title) y el nombre de la compañia.
